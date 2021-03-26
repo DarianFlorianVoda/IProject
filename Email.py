34ef4f025375c00e687d471b6d1c85b4b030768a
Darian <darian.voda00@e-uvt.ro>
2021-03-26 23:25:30 +0200

    Added attachment of the images to the mail and changed the mail subject and body.

--- Email.py
+++ Email.py
@@ -1,11 +1,42 @@
 import smtplib
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
 gmail_user = 'darivoda@gmail.com'
 gmail_password = 'ce506e78'
-
 sent_from = 'darivoda@gmail.com'
 to = ['darian.voda00@e-uvt.ro']
-subject = 'OMG Super Important Message'
-body = "Hey, what's up?\n\n- Darian"
+subject = 'COVID-19 Charts - Project'
+body = "Hello\n\n This is a mail which contains 3 charts relating to COVID-19 cases worldwide. Enjoy!\n\n- Darian"
+
+
+msg = MIMEMultipart()
+msg['From'] = sent_from
+msg['To'] = gmail_user
+msg['Date'] = formatdate(localtime=True)
+msg['Subject'] = subject
+
+msg.attach(MIMEText(body,'plain'))
+
+with open(r'C:\Users\Dari-PC\PycharmProjects\IProject2\Covid19Stats\barchart.png', 'rb') as fp:
+    img = MIMEImage(fp.read())
+    img.add_header('Content-Disposition','attachment',filename=r"C:\Users\Dari-PC\PycharmProjects\IProject2\Covid19Stats\barchart.png")
+
+msg.attach(img)
+
+with open(r'C:\Users\Dari-PC\PycharmProjects\IProject2\Covid19Stats\piechart.png', 'rb') as fp:
+    img = MIMEImage(fp.read())
+    img.add_header('Content-Disposition','attachment',filename=r"C:\Users\Dari-PC\PycharmProjects\IProject2\Covid19Stats\piechart.png")
+
+msg.attach(img)
+
+with open(r'C:\Users\Dari-PC\PycharmProjects\IProject2\Covid19Stats\scatter.png', 'rb') as fp:
+    img = MIMEImage(fp.read())
+    img.add_header('Content-Disposition','attachment',filename=r"C:\Users\Dari-PC\PycharmProjects\IProject2\Covid19Stats\scatter.png")
+
+msg.attach(img)
 
 email_text = """\
 From: %s
@@ -19,7 +50,7 @@ try:
     server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
     server.ehlo()
     server.login(gmail_user, gmail_password)
-    server.sendmail(sent_from, to, email_text)
+    server.sendmail(sent_from, to, msg.as_string())
     server.close()
 except:
     print('Something went wrong...')
